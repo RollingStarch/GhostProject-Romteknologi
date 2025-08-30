1fdef80cd595ac2e32761d106a21a6c8da879d42
RollingStarch <rollingstarch@gmail.com>
2025-08-30 11:37:58 +0200

    Update MVP.ino
    utalizes the camera and send the first few bytes of the .jpg file, can be cross checked for header.

--- ESP-32CAM/MVP/MVP.ino
+++ ESP-32CAM/MVP/MVP.ino
@@ -1,16 +1,35 @@
+#include <Arduino.h>
 #include "esp_camera.h"
 #include "camera_config.h"
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  
-  camera_config_t camera_config = get_default_camera_config();
+  Serial.println("Starting camera...");
 
+  camera_config_t config = get_default_camera_config();
 
+  if(esp_camera_init(&config) != ESP_OK){
+    Serial.println("Camera init failed");
+    return;
+  }
+
+  camera_fb_t * fb = esp_camera_fb_get();
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
+
+  Serial.printf("Picture taken, size: %u bytes\n", fb->len);
+
+  // Print first few bytes
+  for(int i=0; i<16 && i<fb->len; i++){
+    Serial.printf("%02X ", fb->buf[i]);
+  }
+  Serial.println();
+
+  esp_camera_fb_return(fb);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(100000);
 }
