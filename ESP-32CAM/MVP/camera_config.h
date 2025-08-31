66c7a426f3fbe1f12c1fe2e8d4de69b66e0a7a43
RollingStarch <rollingstarch@gmail.com>
2025-08-31 19:16:28 +0200

    Update camera_config.h

--- ESP-32CAM/MVP/camera_config.h
+++ ESP-32CAM/MVP/camera_config.h
@@ -1,24 +1,6 @@
 #pragma once
 #include "esp_camera.h"
-
-// AI Thinker camera config
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM     0
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
-
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      21
-#define Y4_GPIO_NUM      19
-#define Y3_GPIO_NUM      18
-#define Y2_GPIO_NUM       5
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
+#include "board_config.h"
 
 // Function to create default camera config
 inline camera_config_t get_default_camera_config() {
@@ -43,14 +25,18 @@ inline camera_config_t get_default_camera_config() {
     config.pin_reset    = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
     config.pixel_format = PIXFORMAT_JPEG;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
 
     if(psramFound()){
-        config.frame_size = FRAMESIZE_UXGA;  // 1600x1200
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
-    } else {
-        config.frame_size = FRAMESIZE_SVGA;  // 800x600
         config.jpeg_quality = 12;
+        config.frame_size = FRAMESIZE_SVGA;  // UXGA = 1600x1200
+        config.fb_count = 1;
+    } else {
+        config.jpeg_quality = 12;
+        config.frame_size = FRAMESIZE_SVGA;  // SVGA = 800x600
         config.fb_count = 1;
     }
     return config;
