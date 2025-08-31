0de0fc3e0d99af7f24bc91c8b06058246529e8a1
RollingStarch <rollingstarch@gmail.com>
2025-08-31 19:15:59 +0200

    Update MVP.ino

--- ESP-32CAM/MVP/MVP.ino
+++ ESP-32CAM/MVP/MVP.ino
@@ -3,33 +3,29 @@
 #include "camera_config.h"
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting camera...");
+  Serial.begin(9600);
+  //Serial.println("Starting camera...");
 
   camera_config_t config = get_default_camera_config();
 
   if(esp_camera_init(&config) != ESP_OK){
-    Serial.println("Camera init failed");
+    //Serial.println("Camera init failed");
     return;
   }
 
   camera_fb_t * fb = esp_camera_fb_get();
   if(!fb) {
-    Serial.println("Camera capture failed");
+    //Serial.println("Camera capture failed");
     return;
   }
 
-  Serial.printf("Picture taken, size: %u bytes\n", fb->len);
+  //Serial.printf("Picture taken, size: %u bytes\n", fb->len);
 
-  // Print first few bytes
-  for(int i=0; i<16 && i<fb->len; i++){
-    Serial.printf("%02X ", fb->buf[i]);
-  }
-  Serial.println();
+  //Serial.printf("START %u\n", fb->len);
+  Serial.write(fb->buf, fb->len);
 
   esp_camera_fb_return(fb);
 }
 
 void loop() {
-  delay(100000);
 }
